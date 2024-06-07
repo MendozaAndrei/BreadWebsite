a70febbc264c598ec269c7aa4e46b6df082c181c
Padrei_Juan <amendoza39@gmail.com>
2024-06-07 16:47:57 -0700

    Update Layout

---
+++ index.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response } from "express";
+import path from "path";
+import session from "express-session";
+const app = express();
+import expressLayouts from "express-ejs-layouts";
+app.use(expressLayouts);
+
+app.set("view engine", "ejs")
+// app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  }),
+)
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+app.get("/home", (req: Request, res: Response) => {
+    res.render("homepage");
+})
+
+app.listen(3000, () => {
+  console.log("Server is running on port http://localhost:3000");
+});
