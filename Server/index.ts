41759f8d3d86b33942cb776cc7085b6646bba762
tris002xx <tristanjames3131@gmail.com>
2024-06-30 21:34:43 -0700

    reorganizing file structure

--- Server/index.ts
+++ Server/index.ts
@@ -2,14 +2,14 @@ import express, { Request, Response } from "express";
 import path from "path";
 import session from "express-session";
 const app = express();
-import expressLayouts from "express-ejs-layouts";
+
 const livereload = require("livereload");
 const connectLiveReload = require("connect-livereload");
 
 // Allows us to access the .env file
 require('dotenv').config()
 
-app.use(expressLayouts);
+
 app.use(connectLiveReload());
 app.set("view engine", "ejs")
 
@@ -28,14 +28,31 @@ app.use(
 )
 
 
-// Routes
 app.set("view engine", "ejs"); //tells server that we will be using ejs files
 
 
+// Routes
 app.get("/home", (req: Request, res: Response) => {
-  res.render("homepage");
+  res.render("pages/home");
 });
-//npm start to run this bitch up
+app.get("/about", (req: Request, res: Response) => {
+  res.render("pages/about");
+});
+app.get("/services", (req: Request, res: Response) => {
+  res.render("pages/services");
+});
+app.get("/products", (req: Request, res: Response) => {
+  res.render("pages/products");
+});
+app.get("/contact", (req: Request, res: Response) => {
+  res.render("pages/contact");
+});
+
+
+
+
+
+// Server Start
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port http://localhost:${process.env.PORT}`);
